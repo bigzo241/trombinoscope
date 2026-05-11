70d866ca8e63ab5ec2369d62d84f111ca950701c
Mamadou ZO <zomamadou9@gmail.com>
2026-05-11 00:37:10 +0000

    Création du bouton Ajouter un individu et du formulaire d'ajout

--- src/main/java/com/mamadou/trombinoscope/HelloController.java
+++ src/main/java/com/mamadou/trombinoscope/HelloController.java
@@ -1,14 +1,25 @@
 package com.mamadou.trombinoscope;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void ajouterIndividuForm() throws IOException {
+        Stage ajoutForm = new Stage();
+        ajoutForm.initModality(Modality.APPLICATION_MODAL);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ajout-form.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        ajoutForm.setTitle("Formulaire d'ajout");
+        ajoutForm.setScene(scene);
+        ajoutForm.showAndWait();
     }
 }
